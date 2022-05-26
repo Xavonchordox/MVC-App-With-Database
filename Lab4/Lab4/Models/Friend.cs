02c410fb36d287127ed2318fc262c6f4a2ab515c
Xavonchordox <andonovangel1@gmail.com>
2022-05-26 15:38:42 +0200

    Upload file commit

---
+++ Lab4/Lab4/Models/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab4.Models
+{
+    public class Friend
+    {
+        [Required(ErrorMessage = "Please enter ID")]
+        [Range(0, 200)]
+        [DisplayName("ID:")]
+        public int Index { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        [DisplayName("Name:")]
+        public string Ime { get; set; }
+
+        [DisplayName("Place:")]
+        public string MestoZiveenje { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+    }
+}
